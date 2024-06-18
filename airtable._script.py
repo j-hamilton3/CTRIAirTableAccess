6b28e4d2cc7cf5ef09d4b73bbc651670251fda07
General <jhamilton3@rrc.ca>
2024-06-18 14:31:07 -0500

    Added Json pretty printing.

--- airtable._script.py
+++ airtable._script.py
@@ -2,22 +2,20 @@ from airtable import Airtable
 import requests
 import os
 from dotenv import load_dotenv
+import json
 
-# Load environment variables from the .env file
+# Load environment variables from the .env file.
 load_dotenv()
 
-# Replace 'baseKey' and 'table name' with appropriate values
 base_key = 'apphf38ldaaUbUExb'
 table_name = 'tblVfGpBk27WnNufK'
 api_key = os.environ['AIRTABLE_API_KEY']
 
-# Initialize the Airtable client
 airtable = Airtable(base_key, table_name, api_key=api_key)
 
-# Retrieve all data from the 'Main View' and store it in 'table_data'
 try:
-    table_data = airtable.get_all()
+    table_data = airtable.get_all() # Consider accessing specific views later.
     print("Data retrieved successfully.")
-    print(table_data)
+    print(json.dumps(table_data, indent=2))
 except requests.exceptions.HTTPError as e:
     print(f"An error occurred: {e}")
